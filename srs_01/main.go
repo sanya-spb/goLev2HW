5ae5c604011d0b09a2990a69aa11df98b74e5c85
Alexander Kupriyanov <50520613+sanya-spb@users.noreply.github.com>
2021-05-03 19:39:32 +0300

    v3

--- srs_01/main.go
+++ srs_01/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/sanya-spb/goLev2HW/structurer"
+	"reflect"
 )
 
 type confDatabase struct {
@@ -25,10 +25,39 @@ func main() {
 	}
 
 	fmt.Printf("Result before: %+v\n", conf)
-	err := structurer.FillStruct(conf, filler)
+	err := FillStruct(conf, filler)
 	if err != nil {
 		fmt.Printf("error: %s\n", err.Error())
 	}
 	fmt.Printf("Result after: %+v\n", conf)
 
+}
+
+func FillStruct(in interface{}, filler map[string]interface{}) error {
+	if in == nil {
+		return errors.New("arg is nil")
+	}
+
+	if filler == nil {
+		return errors.New("filler is nil")
+	}
+
+	sVal := reflect.ValueOf(in)
+	if sVal.Kind() == reflect.Ptr {
+		sVal = sVal.Elem()
+	}
+
+	if sVal.Type().Kind() != reflect.Struct {
+		return fmt.Errorf("arg is not struct; kind=%s", sVal.Type().Kind())
+	}
+
+	for key, v := range filler {
+		value := sVal.FieldByName(key)
+		newVal := reflect.ValueOf(v)
+		if value.Type().Kind() != newVal.Type().Kind() {
+			return errors.New("type field is not correct")
+		}
+		value.Set(newVal)
+	}
+	return nil
 }
